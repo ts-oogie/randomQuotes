05468d4c54e2c7f526fa21f1677354322783decd
ts-oogie <onagususa@gmail.com>
2023-03-14 04:00:27 -0400

    Removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import  QuoteBox from './components/quoteBox' 
 import {useEffect, useState} from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css' 
 
 
 function App() {
